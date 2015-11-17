b3e59ecd4ca83895c0519d4aea2e018dc91b4d6b
konradhalas <halas.konrad@gmail.com>
2015-11-17 01:11:21 +0100

    Attach token to every request.

--- src/auth.service.js
+++ src/auth.service.js
@@ -6,9 +6,26 @@
     .service('authService', AuthService)
     .provider('authService', AuthServiceProvider);
 
-  function AuthServiceProvider() {
+  function AuthServiceProvider($httpProvider) {
+    var that = this;
+
+    var configureInterceptors = function () {
+      $httpProvider.interceptors.push(
+        ['$q', '$location', 'tokenStorageService', function($q, $location, tokenStorageService) {
+        return {
+          request: function(config) {
+            if (config.url.indexOf(that.config.baseUrl) === 0 && tokenStorageService.has()) {
+              config.headers[that.config.headerName] = that.config.headerPrefix + tokenStorageService.get();
+            }
+            return config || $q.when(config);
+          }
+        };
+      }]);
+    };
+
     this.configure = function (config) {
       this.config = config;
+      configureInterceptors();
     };
 
     this.$get = function($http, tokenStorageService) {
@@ -19,7 +36,7 @@
   function AuthService($http, tokenStorageService, config) {
 
     this.login = function (credentials) {
-      $http.post(config.baseUrl + config.loginPath, credentials).then(function (response) {
+      return $http.post(config.baseUrl + config.loginPath, credentials).then(function (response) {
         tokenStorageService.set(response.data[config.tokenKey]);
       });
     };
--- test/auth.service.spec.js
+++ test/auth.service.spec.js
@@ -2,24 +2,27 @@
   'use strict';
 
   describe('authService', function() {
-    var $httpBackend, authService, tokenStorageService;
+    var $http, $httpBackend, authService, tokenStorageService;
     var config = {
       baseUrl: 'http://test.com',
       loginPath: '/auth/login',
-      tokenKey: 'token'
+      tokenKey: 'token',
+      headerName: 'Authorization',
+      headerPrefix: 'Token '
     };
 
     beforeEach(module('auth-ng', function (authServiceProvider) {
       authServiceProvider.configure(config);
     }));
 
-    beforeEach(inject(function(_authService_,  _$httpBackend_, _tokenStorageService_) {
-      authService = _authService_;
+    beforeEach(inject(function(_$http_, _$httpBackend_, _authService_, _tokenStorageService_) {
+      $http = _$http_;
       $httpBackend =  _$httpBackend_;
+      authService = _authService_;
       tokenStorageService = _tokenStorageService_;
     }));
 
-    it('should login user', function() {
+    it('should obtain and store token', function() {
       var user = {
         'username': 'test',
         'password': 'test'
@@ -27,12 +30,26 @@
       var response = {
         token: '1234'
       };
+      var spy = jasmine.createSpyObj('spy', ['callback']);
       $httpBackend.expectPOST(config.baseUrl + config.loginPath, user).respond(200, response);
 
-      authService.login(user);
+      authService.login(user).then(spy.callback);
 
       $httpBackend.flush();
       expect(tokenStorageService.get()).toBe(response.token);
+      expect(spy.callback).toHaveBeenCalled()
+    });
+
+    it('should add token to request headers', function () {
+      var token = '1234';
+      tokenStorageService.set(token);
+      $httpBackend.expectPOST(config.baseUrl, undefined, function(headers) {
+        return headers[config.headerName] === config.headerPrefix + token;
+      }).respond(200);
+
+      $http.post(config.baseUrl);
+
+      $httpBackend.flush();
     });
 
     afterEach(function() {
